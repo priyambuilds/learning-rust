baf0d8b6e4995652df2d18b3ab6a205706586052
priyambuilds <aichive404@gmail.com>
2025-12-12 15:13:11 +0530

    update

--- src/main.rs
+++ src/main.rs
@@ -57,20 +57,49 @@
 // }
 
 // Iteration
-fn main() {
-    // arrays, maps, Strings
-    let sentence = String::from("my name is priyam");
-    let first_word = get_first_word(sentence);
-    print!("first word is : {}", first_word);
+// fn main() {
+//     // arrays, maps, Strings
+//     let sentence = String::from("my name is priyam");
+//     let first_word = get_first_word(sentence);
+//     print!("first word is : {}", first_word);
+// }
+
+// fn get_first_word(sentence:String) -> String {
+//     let mut ans = String::from("");
+//     for char in sentence.chars() {
+//         ans.push_str(char.to_string().as_str());
+//         if char == ' ' {
+//             break;
+//         }
+//     }
+//     return char;
+// }
+
+// Ownership & borrowing
+// fn main () {
+//     let my_string = String::from("hello world");
+//     borrow_variable(&my_string); // Pass a reference to my_string
+//     println!("{}", my_string); // This is valid because ownership was not transferred
+// }
+
+// fn borrow_variable (some_string: &String) {
+//     println!("{}", some_string); // some_string is borrowed and not moved
+// }
+
+// Structs
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
 }
 
-fn get_first_word(sentence:String) -> String {
-    let mut ans = String::from("");
-    for char in sentence.chars() {
-        ans.push_str(char.to_string().as_str());
-        if char == ' ' {
-            break;
-        }
-    }
-    return char;
+fn main () {
+    let user1 = User {
+        active: true,
+        username: String::from("priyam"),
+        email: String::from("priyam@gmail.com"),
+        sign_in_count: 1,
+    };
+    print!("User 1 username: {:?}", user1.username);
 }
