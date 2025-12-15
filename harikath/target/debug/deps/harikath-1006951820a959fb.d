23b99b127de9737658bb8c4d381c5538c9901364
priyambuilds <aichive404@gmail.com>
2025-12-15 19:59:09 +0530

    project setup

---
+++ harikath/target/debug/deps/harikath-1006951820a959fb.d
@@ -0,0 +1,5 @@
+C:\Users\user\Documents\learning-rust\harikath\target\debug\deps\harikath-1006951820a959fb.d: src\main.rs
+
+C:\Users\user\Documents\learning-rust\harikath\target\debug\deps\libharikath-1006951820a959fb.rmeta: src\main.rs
+
+src\main.rs:
---
+++ harikath/target/debug/deps/harikath-7cfb5ea7f9bdc8f8.d
@@ -0,0 +1,5 @@
+C:\Users\user\Documents\learning-rust\harikath\target\debug\deps\harikath-7cfb5ea7f9bdc8f8.d: src\main.rs
+
+C:\Users\user\Documents\learning-rust\harikath\target\debug\deps\libharikath-7cfb5ea7f9bdc8f8.rmeta: src\main.rs
+
+src\main.rs:
