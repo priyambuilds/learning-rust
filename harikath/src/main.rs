98c2342b58c836af83fed4e62949c93ee564ed2c
priyambuilds <aichive404@gmail.com>
2025-12-17 14:46:59 +0530

    update

--- harikath/src/main.rs
+++ harikath/src/main.rs
@@ -1,170 +1,170 @@
 // Numbers
-// fn main () {
-//     let x: i32 = 32;
-//     let y: u32 = 1000;
-//     let y: f32 = 1000.001;
+fn main() {
+    let x: i32 = 32;
+    let y: u32 = 1000;
+    let y: f32 = 1000.001;
 
-//     print!("x: {}", x);
-// }
+    print!("x: {}", x);
+}
 
 // Booleans
-// fn main () {
-//     let is_male: bool = true;
-//     let is_above_18: bool = true;
+fn main() {
+    let is_male: bool = true;
+    let is_above_18: bool = true;
 
-//     if is_male {
-//         print!("You are a male.");
-//     } else {
-//         print!("You are not a male.");
-//     }
+    if is_male {
+        print!("You are a male.");
+    } else {
+        print!("You are not a male.");
+    }
 
-//     if is_male && is_above_18 {
-//         print!("you are a legal male")
-//     }
-// }
+    if is_male && is_above_18 {
+        print!("you are a legal male")
+    }
+}
 
 // Strings
-// fn main () {
-//     let greeting = String::from("hello world");
-//     println!("{}", greeting);
+fn main() {
+    let greeting = String::from("hello world");
+    println!("{}", greeting);
 
-//     let char1 = greeting.chars().nth(1000);
+    let char1 = greeting.chars().nth(1000);
 
-//     match char1 {
-//         Some(c) => println!("char: {}", c),
-//         None => println!("No character at index 1000"),
-//     }
+    match char1 {
+        Some(c) => println!("char: {}", c),
+        None => println!("No character at index 1000"),
+    }
 
-//     print!("{}", char1)
-// }
+    print!("{}", char1)
+}
 
 // Conditionals
-// fn main () {
-//     let is_even = true;
+fn main() {
+    let is_even = true;
 
-//     if (is_even) {
-//         println!("even");
-//     } else if !is_even {
-//         println!("odd");
-//     }
-// }
+    if is_even {
+        println!("even");
+    } else if !is_even {
+        println!("odd");
+    }
+}
 
 // Loops
-// fn main() {
-//     for i in 0..10 {
-//         print!("{}", i);
-//     }
-// }
+fn main() {
+    for i in 0..10 {
+        print!("{}", i);
+    }
+}
 
 // Iteration
-// fn main() {
-//     // arrays, maps, Strings
-//     let sentence = String::from("my name is priyam");
-//     let first_word = get_first_word(sentence);
-//     print!("first word is : {}", first_word);
-// }
+fn main() {
+    // arrays, maps, Strings
+    let sentence = String::from("my name is priyam");
+    let first_word = get_first_word(sentence);
+    print!("first word is : {}", first_word);
+}
 
-// fn get_first_word(sentence:String) -> String {
-//     let mut ans = String::from("");
-//     for char in sentence.chars() {
-//         ans.push_str(char.to_string().as_str());
-//         if char == ' ' {
-//             break;
-//         }
-//     }
-//     return char;
-// }
+fn get_first_word(sentence: String) -> String {
+    let mut ans = String::from("");
+    for char in sentence.chars() {
+        ans.push_str(char.to_string().as_str());
+        if char == ' ' {
+            break;
+        }
+    }
+    return char;
+}
 
 // Ownership & borrowing
-// fn main () {
-//     let my_string = String::from("hello world");
-//     borrow_variable(&my_string); // Pass a reference to my_string
-//     println!("{}", my_string); // This is valid because ownership was not transferred
-// }
+fn main() {
+    let my_string = String::from("hello world");
+    borrow_variable(&my_string); // Pass a reference to my_string
+    println!("{}", my_string); // This is valid because ownership was not transferred
+}
 
-// fn borrow_variable (some_string: &String) {
-//     println!("{}", some_string); // some_string is borrowed and not moved
-// }
+fn borrow_variable(some_string: &String) {
+    println!("{}", some_string); // some_string is borrowed and not moved
+}
 
 // Structs
-// struct User {
-//     active: bool,
-//     username: String,
-//     email: String,
-//     sign_in_count: u64,
-// }
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
 
-// fn main () {
-//     let user1 = User {
-//         active: true,
-//         username: String::from("priyam"),
-//         email: String::from("priyam@gmail.com"),
-//         sign_in_count: 1,
-//     };
-//     print!("User 1 username: {:?}", user1.username);
-// }
+fn main() {
+    let user1 = User {
+        active: true,
+        username: String::from("priyam"),
+        email: String::from("priyam@gmail.com"),
+        sign_in_count: 1,
+    };
+    print!("User 1 username: {:?}", user1.username);
+}
 
 // Implementing structs
-// struct Rect {
-//     width: u32,
-//     height: u32,
-// }
+struct Rect {
+    width: u32,
+    height: u32,
+}
 
-// impl Rect {
-//     fn area(&self) -> u32 {
-//         return self.width * self.height;
-//     }
-//     fn perimeter(&self) -> u32 {
-//         return 2* (self.width + self.height);
-//     }
-// }
+impl Rect {
+    fn area(&self) -> u32 {
+        return self.width * self.height;
+    }
+    fn perimeter(&self) -> u32 {
+        return 2 * (self.width + self.height);
+    }
+}
 
-// fn main () {
-//     let rect = Rect {
-//         width: 30,
-//         height: 50,
-//     };
-//     println!("area: {}", rect.area());
-//     println!("perimeter: {}", rect.perimeter());
-// }
+fn main() {
+    let rect = Rect {
+        width: 30,
+        height: 50,
+    };
+    println!("area: {}", rect.area());
+    println!("perimeter: {}", rect.perimeter());
+}
 
 // Enums
-// enum Direction {
-//     North,
-//     South,
-//     East,
-//     West,
-// }
+enum Direction {
+    North,
+    South,
+    East,
+    West,
+}
 
-// fn main () {
-//     let my_direction = Direction::North;
-//     move_around(my_direction);
-// }
+fn main() {
+    let my_direction = Direction::North;
+    move_around(my_direction);
+}
 
-// fn move_around(direction: Direction) {
-//     // implements logic to move a character around
-// }
+fn move_around(direction: Direction) {
+    // implements logic to move a character around
+}
 
 // Enums with values
 // define an enum called shape
-// enum Shape {
-//     Circle(f64), // Variant with associated data (radius)
-//     Rectangle(f64, f64), // Variant with associated data (width, height)
-//     Square(f64), // Variant with associated data (side length)
-// }
+enum Shape {
+    Circle(f64), // Variant with associated data (radius)
+    Rectangle(f64, f64), // Variant with associated data (width, height)
+    Square(f64), // Variant with associated data (side length)
+}
 
-// // Function to calculate area based on the shape
+// Function to calculate area based on the shape
 // fn calculate_area(shape: Shape) -> f64 {
-//     // calculate area based of the shape
+// calculate area based of the shape
 //     return 0.00;
 // }
 
-// fn main () {
-//     // Create instances of different shapes
-//     let circle = Shape::Circle(5.0);
-//     let rectangle = Shape::Rectangle(3.0, 4.0);
-//     let square = Shape::Square(2.0);
-// }
+fn main() {
+    // Create instances of different shapes
+    let circle = Shape::Circle(5.0);
+    let rectangle = Shape::Rectangle(3.0, 4.0);
+    let square = Shape::Square(2.0);
+}
 
 // Pattern matching
 // enum Shape {
--- rust_book/miscellenious/src/main.rs
+++ rust_book/miscellenious/src/main.rs
@@ -1,3 +1,44 @@
+// Returning values from loops
 fn main() {
-    println!("Hello, world!");
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+    println!("The result is {result}");
+}
+
+// Loop within loops
+// Disambiguating with Loop Labels
+fn main() {
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {}", count);
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {}", remaining);
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+        count += 1;
+    }
+    println!("End count = {}", count);
+}
+
+// Using for loop
+fn main() {
+    for numebr in (1..4).rev() {
+        println!("{number}");
+    }
+    println!("Hello from main");
 }
