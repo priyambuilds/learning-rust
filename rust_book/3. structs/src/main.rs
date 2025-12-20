d00203fb8ed872c69a6bf9665b4f89da166fd548
priyambuilds <aichive404@gmail.com>
2025-12-20 23:42:21 +0530

    update

--- rust_book/3. structs/src/main.rs
+++ rust_book/3. structs/src/main.rs
@@ -40,3 +40,41 @@ fn main() {
 fn area(rectangle: &Rectangle) -> u32 {
     rectangle.width * rectangle.height
 }
+
+// Method Syntax
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle { width: 30, height: 50 };
+
+    println!("The area of the rectangle is {} square pixels.", rect1.area());
+}
+
+// Methods with more parameters
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    
+    fn can_hold(&self other:Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+fn main() {
+    let rec1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 50, height: 60 };
+    let rect3 = Rectangle { width: 10, height: 40 };
+
+    peintln!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    peintln!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+}
